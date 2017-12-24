8724f1f6cf69c607e53979b742a38e4eaa4677b5
Mohit <Mohit@192.168.1.7>
2017-12-25 00:49:59 +0530

    All working CRUD functions

--- SpringBootRemote/src/main/java/spring/controller/AdminController.java
+++ SpringBootRemote/src/main/java/spring/controller/AdminController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.model.Admin;
@@ -36,12 +39,45 @@ public class AdminController {
 		return collection;
 	}
 	
-	@RequestMapping(value="/{id}")
-	public Admin getByKey(@PathVariable int id){
+	@RequestMapping(value="/findById/{id}")
+	public Admin getById(@PathVariable int id){
 		Admin admin =  service.getByKey(id);
 		System.out.println(admin.getUserName());
 		return admin;
 	}
+	
+	@PostMapping("/saveOrUpdateAdmin")
+	public Admin saveOrUpdate(@RequestBody Admin admin){
+		Admin resultAdmin =  service.saveOrUpdateAdmin(admin);
+		System.out.println(admin.getUserName());
+		return resultAdmin;
+	}
+	
+	/**
+	 * @param admin
+	 * @return This method always return true even if we pass a record which is not present in the database.
+	 * 
+	 * As of now solution could be to use entity manager directly inside Service class.
+	 */
+	@RequestMapping(value = "/deleteAdmin", method=RequestMethod.DELETE)
+	public boolean delete(@RequestBody Admin admin){
+		boolean deleted =  service.deleteAdmin(admin);
+		return deleted;
+	}
+	
+	
+	@RequestMapping(value = "/deleteAdminEm", method=RequestMethod.DELETE)
+	public boolean deleteEm(@RequestBody Admin admin){
+		boolean deleted =  service.deleteAdminEm(admin);
+		return deleted;
+	}
+	
+	@RequestMapping(value = "/deleteAdminById", method=RequestMethod.DELETE)
+	public boolean deleteAdminbyId(@RequestBody Admin admin){
+		boolean deleted =  service.deleteAdminbyId(admin);
+		return deleted;
+	}
+	
 	
 	@RequestMapping("/alive")
 	public String aliveCheck(){
@@ -49,5 +85,4 @@ public class AdminController {
 		System.out.println(message);
 		return message;
 	}
-
 }
--- SpringBootRemote/src/main/java/spring/service/AdminService.java
+++ SpringBootRemote/src/main/java/spring/service/AdminService.java
@@ -3,14 +3,18 @@ package spring.service;
 import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import spring.dao.DummyDao;
 import spring.model.Admin;
 import spring.repository.AdminRepository;
 
 @Service
+@Transactional
 public class AdminService {
 	
 	
@@ -19,6 +23,9 @@ public class AdminService {
 	
 	@Autowired
 	private AdminRepository adminRepository;
+	
+	@Autowired
+	private EntityManager em;
 	
 	
     public List<Admin> getAllAdmin() {
@@ -33,6 +40,46 @@ public class AdminService {
 		
 		return adminRepository.getOne(id);
 	}
+
+	/**
+	 * @param admin
+	 * 
+	 * This method will create db entry if it dosent exist and update the entity if it exits.
+	 * @return
+	 */
+	public Admin saveOrUpdateAdmin(Admin admin) {
+		
+		return adminRepository.saveAndFlush(admin);
+	}
 	
+
+	/**
+	 * @param admin
+	 * @return This method executes even an entity is deleted. 
+	 */
+	public boolean deleteAdmin(Admin admin) {
+		adminRepository.delete(admin);
+		return true;
+	}
+	
+	public boolean deleteAdminbyId(Admin admin) {
+		adminRepository.delete(admin.getAdminId());
+		return true;
+	}
+	
+	public boolean deleteAdminEm(Admin admin){
+		boolean deleted = false;
+		try{
+			em.remove(admin);
+			em.flush();
+			deleted = true;
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		finally{
+		}
+		return deleted;
+	}
 	
 }
