29073cd86c6f20408e154a8419e2df0b37ed7443
Mohit <Mohit@192.168.1.4>
2018-01-01 00:54:53 +0530

    Eureka service project

---
+++ Service_Registry/src/main/java/spring/server/eureka/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package spring.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableEurekaServer
+@SpringBootApplication
+@ComponentScan(basePackages = "spring")
+public class ServiceRegistryApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
